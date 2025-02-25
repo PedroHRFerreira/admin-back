715679ef0cde8c05edebcc149a21e4c120597c42
PedroHRFerreira <devpedrohr@gmail.com>
2025-02-24 22:39:22 -0300

    fix: :bug: filtro sales

--- app/Http/Controllers/Api/SaleController.php
+++ app/Http/Controllers/Api/SaleController.php
@@ -7,72 +7,86 @@ use Illuminate\Http\Request;
 
 class SaleController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $months = [
+            [
+                'label' => 'JANEIRO',
+                'value' => 0,
+                'product' => '',
+            ],
+            [
+                'label' => 'FEVEREIRO',
+                'value' => 0,
+                'product' => '',
+            ],
+            [
+                'label' => 'MARÇO',
+                'value' => 0,
+                'product' => '',
+            ],
+            [
+                'label' => 'ABRIL',
+                'value' => 0,
+                'product' => '',
+            ],
+            [
+                'label' => 'MAIO',
+                'value' => 0,
+                'product' => '',
+            ],
+            [
+                'label' => 'JUNHO',
+                'value' => 0,
+                'product' => '',
+            ],
+            [
+                'label' => 'JULHO',
+                'value' => 0,
+                'product' => '',
+            ],
+            [
+                'label' => 'AGOSTO',
+                'value' => 0,
+                'product' => '',
+            ],
+            [
+                'label' => 'SETEMBRO',
+                'value' => 0,
+                'product' => '',
+            ],
+            [
+                'label' => 'OUTUBRO',
+                'value' => 0,
+                'product' => '',
+            ],
+            [
+                'label' => 'NOVEMBRO',
+                'value' => 0,
+                'product' => '',
+            ],
+            [
+                'label' => 'DEZEMBRO',
+                'value' => 0,
+                'product' => '',
+            ],
+        ];
+
+        if ($request->filled('with')) {
+            $monthParam = strtoupper($request->input('with'));
+            
+            $filtered = collect($months)->firstWhere('label', $monthParam);
+
+            if (!$filtered) {
+                return response()->json(['error' => 'Mês não encontrado'], 404);
+            }
+
+            $months = $filtered;
+        }
+
         $data = [
             'sale' => [
-                'month' => [
-                    [
-                        'label' => 'JANEIRO',
-                        'value' => 0,
-                        'product' => '',
-                    ],
-                    [
-                        'label' => 'FEVEREIRO',
-                        'value' => 0,
-                        'product' => '',
-                    ],
-                    [
-                        'label' => 'MARÇO',
-                        'value' => 0,
-                        'product' => '',
-                    ],
-                    [
-                        'label' => 'ABRIL',
-                        'value' => 0,
-                        'product' => '',
-                    ],
-                    [
-                        'label' => 'MAIO',
-                        'value' => 0,
-                        'product' => '',
-                    ],
-                    [
-                        'label' => 'JUNHO',
-                        'value' => 0,
-                        'product' => '',
-                    ],
-                    [
-                        'label' => 'JULHO',
-                        'value' => 0,
-                        'product' => '',
-                    ],
-                    [
-                        'label' => 'AGOSTO',
-                        'value' => 0,
-                        'product' => '',
-                    ],
-                    [
-                        'label' => 'SETEMBRO',
-                        'value' => 0,
-                        'product' => '',
-                    ],
-                    [
-                        'label' => 'OUTUBRO',
-                        'value' => 0,
-                        'product' => '',
-                    ],
-                    [
-                        'label' => 'NOVEMBRO',
-                        'value' => 0,
-                        'product' => '',
-                    ],
-                    [
-                        'label' => 'DEZEMBRO',
-                        'value' => 0,
-                        'product' => '',
-                    ],
-                ],
+                'month' => $months,
             ],
         ];
 
